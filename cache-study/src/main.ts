f2c0f3509bd49010b4c3bd8bd44fbf3451c22bcf
Carlos Zulbaran <czulbaran@supre.com.co>
2023-10-03 10:06:28 -0500

    feat: implement eslint

--- cache-study/src/main.ts
+++ cache-study/src/main.ts
@@ -4,15 +4,15 @@ Created by Deimer Mendez 29/8/23
 @date : 29/8/23
 */
 
-import { GeoLRUCache } from './cache/geo-lru-cache';
+import { GeoLRUCache } from './cache/geo-lru-cache'
 
-const main = async () => {
-  const geoLRUCache = new GeoLRUCache<string, number>(5, 5000);
-  geoLRUCache.put('a', 10, 40.7128, -74.006);
-  geoLRUCache.put('b', 20, 34.0522, -118.2437);
-  geoLRUCache.put('c', 30, 51.5074, -0.1278);
-  geoLRUCache.put('d', 40, 48.8566, 2.3522);
-  geoLRUCache.put('e', 50, 35.682839, 139.759455);
-};
+const main = async (): Promise<void> => {
+  const geoLRUCache = new GeoLRUCache<string, number>(5, 5000)
+  geoLRUCache.put('a', 10, 40.7128, -74.006)
+  geoLRUCache.put('b', 20, 34.0522, -118.2437)
+  geoLRUCache.put('c', 30, 51.5074, -0.1278)
+  geoLRUCache.put('d', 40, 48.8566, 2.3522)
+  geoLRUCache.put('e', 50, 35.682839, 139.759455)
+}
 
-main();
+void main()
